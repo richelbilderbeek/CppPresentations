3c4d99d0c2a7ab2431cf632a4446d2bc8bf6de80
richelbilderbeek <richel@richelbilderbeek.nl>
2015-10-05 21:07:19 +0200

    Created function design 1

--- FunctionDesign/build.sh
+++
@@ -1,2 +0,0 @@
-pandoc -t beamer cpp_function_design.md -V theme:Warsaw -o cpp_function_design1.pdf
-
---
+++ cpp_function_design/build.sh
@@ -0,0 +1,3 @@
+pandoc -t beamer cpp_function_design1.md -V theme:Warsaw -o cpp_function_design1.pdf
+mv cpp_function_design1.pdf ../cpp_function_design1.pdf
+
